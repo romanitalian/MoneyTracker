0583e13db6a7ca08ff8e50d65edae26f8b0a19c1
romanitalian <romanitalian@yandex.ru>
2015-04-28 00:22:34 +0300

    try to user: Bundle() -in activity and fragment

--- app/src/main/java/net/romanitalian/moneytrackerapp/MainActivity.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/MainActivity.java
@@ -11,16 +11,14 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setBoundles();
         setContentView(R.layout.activity_main);
-
-        this.setBoundles();
     }
 
     public void setBoundles() {
-        Bundle bundle = new Bundle();
-        bundle.putString("date_format", this.date_format);
-
         TransactionsFragment fragment = new TransactionsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("date_format", date_format);
         fragment.setArguments(bundle);
     }
 }
