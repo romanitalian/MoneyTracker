d16c65052b47ed61926f4021f0c78977b0dc34a4
romanitalian <romanitalian@yandex.ru>
2015-04-28 21:26:09 +0300

    try to user Bundle - see block by tags in comment code: // #try_bundle activity // #try_bundle fragment

--- app/src/main/java/net/romanitalian/moneytrackerapp/TransactionsFragment.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/TransactionsFragment.java
@@ -39,6 +39,9 @@ public class TransactionsFragment extends Fragment {
     }
 
     private List<Transactions> getTransactions() {
+        // #try_bundle fragment
+        String date_format = getArguments().getString("date_format");
+
         DateFormat df = new SimpleDateFormat("dd-MM-yyyy", new Locale("ru"));
         Date now_calendar = Calendar.getInstance().getTime();
         String now = df.format(now_calendar);
