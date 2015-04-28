015d7f7bc1b5c4157995114f602fa3258bed8f02
Roman Romadin <romadin@immo.ru>
2015-04-28 23:27:09 +0300

    show not working code

--- app/src/main/java/net/romanitalian/moneytrackerapp/TransactionsFragment.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/TransactionsFragment.java
@@ -29,21 +29,28 @@ public class TransactionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View inflate = inflater.inflate(R.layout.fragment_transactions, container);
+
+//        List<Transactions> transactions = getTransactions(this.date_format);
+//        TransactionAdapter adapter = new TransactionAdapter(getActivity(), transactions);
+//        ListView list = (ListView) inflate.findViewById(R.id.list_view_id);
+//        list.setAdapter(adapter);
+//
         List<Transactions> adapterData = getTransactions();
-
         transactionAdapter = new TransactionAdapter(getActivity(), adapterData);
-
         listView = (ListView) inflate.findViewById(R.id.list_view_id);
         listView.setAdapter(transactionAdapter);
 
         return inflate;
     }
 
-    private List<Transactions> getTransactions() {
+    private List<Transactions> getTransactions(/*String format*/) {
         // #try_bundle fragment
 //        String date_format = getArguments().getString("date_format");
 
-        DateFormat df = new SimpleDateFormat(this.date_format, new Locale("ru"));
+        Bundle bundle = getArguments();
+        String date_format_new = bundle.getString("date_format");
+
+        DateFormat df = new SimpleDateFormat(date_format_new, new Locale("ru"));
         Date now_calendar = Calendar.getInstance().getTime();
         String now = df.format(now_calendar);
         data.add(new Transactions("Huawei", "9800", now));
