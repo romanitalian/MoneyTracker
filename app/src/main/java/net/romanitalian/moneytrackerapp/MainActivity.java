2791ccca9beda94a843200d8657d97a367ccf42c
Roman Romadin <romadin@immo.ru>
2015-04-28 21:26:09 +0300

    try to user Bundle - see block by tags in comment code: // #try_bundle activity // #try_bundle fragment

--- app/src/main/java/net/romanitalian/moneytrackerapp/MainActivity.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/MainActivity.java
@@ -1,5 +1,6 @@
 package net.romanitalian.moneytrackerapp;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
@@ -11,6 +12,15 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setFragmentParams();
         setContentView(R.layout.activity_main);
+    }
+
+    public void setFragmentParams() {
+        // #try_bundle activity
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.fragment_transactions_id);
+        Bundle bundle = new Bundle();
+        bundle.putString("date_format", date_format);
+        fragment.setArguments(bundle);
     }
 }
