1ed4f45f360049c994a0d598fa4f9b1bb951f239
romanitalian <romanitalian@yandex.ru>
2015-04-28 09:36:56 +0300

    remove Bundle, change background row color of list transactions, rename app

--- app/src/main/java/net/romanitalian/moneytrackerapp/MainActivity.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/MainActivity.java
@@ -11,14 +11,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setBoundles();
         setContentView(R.layout.activity_main);
-    }
-
-    public void setBoundles() {
-        TransactionsFragment fragment = new TransactionsFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString("date_format", date_format);
-        fragment.setArguments(bundle);
     }
 }
--- app/src/main/java/net/romanitalian/moneytrackerapp/TransactionsFragment.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/TransactionsFragment.java
@@ -23,7 +23,6 @@ public class TransactionsFragment extends Fragment {
     private ListView listView;
     private TransactionAdapter transactionAdapter;
     List<Transactions> data = new ArrayList<>();
-    Bundle args;
 
     @Nullable
     @Override
@@ -40,10 +39,8 @@ public class TransactionsFragment extends Fragment {
     }
 
     private List<Transactions> getDataList() {
-        Bundle bundle = this.getArguments();
-        String date_format = bundle.getString("date_format");
 
-        DateFormat df = new SimpleDateFormat(date_format, new Locale("ru"));
+        DateFormat df = new SimpleDateFormat("dd-MM-yyyy", new Locale("ru"));
         Date now_calendar = Calendar.getInstance().getTime();
         String now = df.format(now_calendar);
         data.add(new Transactions("Huawei", "9800", now));
