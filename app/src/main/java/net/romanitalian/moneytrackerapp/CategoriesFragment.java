e41df5bc475564eaa18b085d156af6d4892a4064
Roman Romadin <romadin@immo.ru>
2015-04-30 19:13:06 +0300

    set basic fitures for use menus: navigation drawer, action bar. And set toggle for call navigation drawer

--- app/src/main/java/net/romanitalian/moneytrackerapp/CategoriesFragment.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/CategoriesFragment.java
@@ -16,7 +16,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class TransactionsFragment extends Fragment {
+public class CategoriesFragment extends Fragment {
     private ListView listView;
     private TransactionAdapter transactionAdapter;
     List<Transaction> data = new ArrayList<>();
