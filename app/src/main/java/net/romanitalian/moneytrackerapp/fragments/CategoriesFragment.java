488804dfb555be947a8367a06fb47fed56c4536f
romanitalian <romanitalian@yandex.ru>
2015-05-06 14:18:02 +0300

    fixed errors, set basic futures in this branch - use drawer layout (add to user interface left menu) and set content for ever fragments

--- app/src/main/java/net/romanitalian/moneytrackerapp/fragments/CategoriesFragment.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/fragments/CategoriesFragment.java
@@ -6,22 +6,37 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import net.romanitalian.moneytrackerapp.R;
+import net.romanitalian.moneytrackerapp.adapters.CategoryAdapter;
+import net.romanitalian.moneytrackerapp.models.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoriesFragment extends Fragment {
+    private ListView listView;
+    private CategoryAdapter categoryAdapter;
+    List<Category> data = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View inflate = inflater.inflate(R.layout.fragment_categories, container, false);
 
-        LinearLayout ll = (LinearLayout) inflate.findViewById(R.id.category_linear_layout);
-        TextView text_view = (TextView) ll.findViewById(R.id.category_id);
-        text_view.setText(R.string.categories_text);
+        List<Category> adapterData = getCategories();
+        categoryAdapter = new CategoryAdapter(getActivity(), adapterData);
+        listView = (ListView) inflate.findViewById(R.id.category_list_view_id);
+        listView.setAdapter(categoryAdapter);
 
         return inflate;
+    }
+
+    private List<Category> getCategories() {
+        data.add(new Category("Category_01"));
+        data.add(new Category("Category_02"));
+        data.add(new Category("Category_03"));
+        return data;
     }
 }
