e22ed6e2275408edb0df64cada3377decfeb621c
Roman Romadin <romadin@immo.ru>
2015-05-06 13:18:29 +0300

    fixed errors, set basic futures in this branch - use drawer layout (add to user interface left menu) and set content for ever fragments

--- app/src/main/java/net/romanitalian/moneytrackerapp/fragments/StatisticsFragment.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/fragments/StatisticsFragment.java
@@ -6,37 +6,22 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import net.romanitalian.moneytrackerapp.R;
-import net.romanitalian.moneytrackerapp.adapters.StatisticAdapter;
-import net.romanitalian.moneytrackerapp.models.Statistic;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class StatisticsFragment extends Fragment {
-    private ListView listView;
-    private StatisticAdapter categoryAdapter;
-    List<Statistic> data = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View inflate = inflater.inflate(R.layout.fragment_statistics, container, false);
 
-        List<Statistic> adapterData = getCategories();
-        categoryAdapter = new StatisticAdapter(getActivity(), adapterData);
-        listView = (ListView) inflate.findViewById(R.id.statistics_id);
-        listView.setAdapter(categoryAdapter);
+        LinearLayout ll = (LinearLayout) inflate.findViewById(R.id.statistic_linear_layout);
+        TextView text_view = (TextView) ll.findViewById(R.id.statistics_id);
+        text_view.setText(R.string.statistics_text);
 
         return inflate;
-    }
-
-    private List<Statistic> getCategories() {
-        data.add(new Statistic("Statistic_01"));
-        data.add(new Statistic("Statistic_02"));
-        data.add(new Statistic("Statistic_03"));
-        return data;
     }
 }
