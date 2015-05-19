4fb19cdb0ac4dfdd81d6d422a96ab407569498ff
Roman Romadin <romadin@immo.ru>
2015-05-19 22:26:07 +0300

    use Drawer - library from github

--- app/src/main/java/net/romanitalian/moneytrackerapp/activities/MainActivity.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/activities/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.mikepenz.materialdrawer.Drawer;
@@ -38,24 +37,9 @@ public class MainActivity extends ActionBarActivity {
             setSupportActionBar(toolbar);
         }
 
-//        AccountHeader.Result headerResult = new AccountHeader()
-//                .withActivity(this)
-//                .withHeaderBackground(R.color.green)
-//                .addProfiles(
-//                        new ProfileDrawerItem().withName("RomanItalian").withEmail("romanitalian.net@gmail.com").withIcon(getResources().getDrawable(R.drawable.android_app_back)),
-//                        new ProfileDrawerItem().withName("asdfasdf").withEmail("5463456345.net@gmail.com").withIcon(getResources().getDrawable(R.drawable.android_app_back))
-//                )
-//                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-//                    @Override
-//                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
-//                        return false;
-//                    }
-//                })
-//                .build();
-        Drawer.Result drawer = new Drawer()
+        new Drawer()
                 .withActivity(this)
                 .withToolbar(toolbar)
-//                .withAccountHeader(headerResult)
                 .addDrawerItems(
                         new PrimaryDrawerItem().withName(R.string.transactions_title),
                         new PrimaryDrawerItem().withName(R.string.categories_title),
@@ -76,10 +60,6 @@ public class MainActivity extends ActionBarActivity {
         // set toggle
         drawerToggle = new ActionBarDrawerToggle(this, drawerWidget, toolbar, R.string.app_name, R.string.app_name);
         drawerWidget.setDrawerListener(drawerToggle);
-
-//        initMenu();
-//        setTitle(getString(R.string.transactions_title));
-//        setFragment(0);
     }
 
     public void setFragmentParams(Fragment fragment) {
@@ -87,32 +67,6 @@ public class MainActivity extends ActionBarActivity {
         bundle.putString("dateFormat", dateFormat);
         fragment.setArguments(bundle);
         getFragmentManager().beginTransaction().replace(R.id.drawer_layout_frame, fragment).commit();
-    }
-
-    public void initMenu() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-        }
-
-        drawerWidget = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerList = (ListView) findViewById(R.id.drawer_list);
-
-        String[] menuLabels = new String[]
-                {
-                        getString(R.string.transactions_title),
-                        getString(R.string.categories_title),
-                        getString(R.string.statistics_title)
-                };
-        ArrayAdapter<String> menu_labels_adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_expandable_list_item_1, menuLabels);
-        drawerList.setAdapter(menu_labels_adapter);
-
-        // set onClickListener on left menu
-        drawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-        // set toggle
-        drawerToggle = new ActionBarDrawerToggle(this, drawerWidget, toolbar, R.string.app_name, R.string.app_name);
-        drawerWidget.setDrawerListener(drawerToggle);
     }
 
     @Override
@@ -143,15 +97,4 @@ public class MainActivity extends ActionBarActivity {
                 break;
         }
     }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            drawerList.setItemChecked(position, true);
-            drawerWidget.closeDrawer(drawerList);
-            setFragment(position);
-        }
-    }
 }
-
-
