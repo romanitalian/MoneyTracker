c71c9ccf5ab887a5a507d8c4f384bd302cd07a4e
roman <romanitalian@yandex.ru>
2015-06-22 20:06:21 +0300

    set animation swipe transactions - remove from activity; update gradle dependency

--- app/src/main/java/net/romanitalian/moneytrackerapp/fragments/TransactionsFragment.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/fragments/TransactionsFragment.java
@@ -12,6 +12,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -72,6 +73,21 @@ public class TransactionsFragment extends Fragment {
                 loadTransactions("");
             }
         });
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder viewHolder1) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int i) {
+//                @todo remove from db
+                transactionAdapter.removeItem(viewHolder.getAdapterPosition());
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(transactionList);
     }
 
     @Override
