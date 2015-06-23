a1bf754a676d8c95387da2ae9ddac272e89a071a
roman <romanitalian@yandex.ru>
2015-06-24 02:46:35 +0300

    add demo chart: EazeGraph

--- app/src/main/java/net/romanitalian/moneytrackerapp/fragments/StatisticsFragment.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/fragments/StatisticsFragment.java
@@ -1,20 +1,20 @@
 package net.romanitalian.moneytrackerapp.fragments;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.widget.LinearLayout;
-
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
 
 import net.romanitalian.moneytrackerapp.R;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @EFragment(R.layout.fragment_statistics)
 public class StatisticsFragment extends Fragment {
@@ -25,66 +25,23 @@ public class StatisticsFragment extends Fragment {
     @ViewById(R.id.chart)
     PieChart chart;
 
-    float[] datapoints = {450, 1290, 300, 500};
+    Map<String, Float> map = new HashMap<>();
 
     @AfterViews
     void ready() {
-//        statistic.setText(R.string.statistics_text);
-//        pieChartView.setDatapoints(datapoints);
+        map.put("Food", 15.7f);
+        map.put("Closes", 35.0f);
+        map.put("Electricity", 25.0f);
+        map.put("Rent", 9.1f);
+        Random r;
+        for (Map.Entry<String, Float> entry : map.entrySet()) {
+            String key = entry.getKey();
+            Float value = entry.getValue();
 
-        chart = configureChart(chart);
-        chart = setData(chart);
-        chart.animateXY(1500, 1500);
-    }
-
-
-    public PieChart configureChart(PieChart chart) {
-        chart.setHoleColor(getResources().getColor(android.R.color.background_dark));
-        chart.setHoleRadius(120f);
-        chart.setDescription("Статистика по категориям");
-        chart.setTransparentCircleRadius(5f);
-//        chart.setDrawYValues(true);
-        chart.setDrawCenterText(true);
-        chart.setDrawHoleEnabled(false);
-        chart.setRotationAngle(0);
-//        chart.setDrawXValues(false);
-        chart.setRotationEnabled(true);
-        chart.setUsePercentValues(true);
-        chart.setCenterText("MoneyTracker");
-        return chart;
-    }
-
-    private PieChart setData(PieChart chart) {
-        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
-        yVals1.add(new Entry(10, 0));
-        yVals1.add(new Entry(2, 1));
-        yVals1.add(new Entry(20, 2));
-        yVals1.add(new Entry(15, 3));
-        yVals1.add(new Entry(7, 4));
-        yVals1.add(new Entry(17, 5));
-        ArrayList<String> xVals = new ArrayList<String>();
-        xVals.add("Food");
-        xVals.add("Electricity");
-        xVals.add("Travel");
-        xVals.add("Medical/Dental");
-        xVals.add("Closing");
-        xVals.add("Rent");
-        PieDataSet set1 = new PieDataSet(yVals1, "sdfgs");
-        set1.setSliceSpace(0f);
-        ArrayList<Integer> colors = new ArrayList<Integer>();
-        colors.add(getResources().getColor(android.R.color.holo_green_light));
-        colors.add(getResources().getColor(android.R.color.holo_red_light));
-        colors.add(getResources().getColor(android.R.color.holo_blue_bright));
-        colors.add(getResources().getColor(android.R.color.holo_orange_dark));
-        colors.add(getResources().getColor(android.R.color.holo_blue_dark));
-        colors.add(getResources().getColor(android.R.color.holo_red_dark));
-        colors.add(getResources().getColor(android.R.color.holo_green_dark));
-        set1.setColors(colors);
-
-        PieData data = new PieData(xVals, set1);
-        chart.setData(data);
-        chart.highlightValues(null);
-        chart.invalidate();
-        return chart;
+            r = new Random();
+            int color = Color.argb(100, r.nextInt(256), r.nextInt(256), r.nextInt(256));
+            chart.addPieSlice(new PieModel(key, value, color));
+        }
+        chart.startAnimation();
     }
 }
