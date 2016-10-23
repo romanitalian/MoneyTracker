5f2aa2af287a3005c8722eb32cb64eaf0c3e0fb8
Roman <romanitalian@yandex.ru>
2016-10-23 12:06:01 +0300

    Change server address

--- app/src/main/java/net/romanitalian/moneytrackerapp/rest/RestClient.java
+++ app/src/main/java/net/romanitalian/moneytrackerapp/rest/RestClient.java
@@ -4,7 +4,8 @@ import org.androidannotations.annotations.rest.Get;
 import org.androidannotations.annotations.rest.Post;
 import org.androidannotations.annotations.rest.Rest;
 
-@Rest(rootUrl = "http://62.109.17.114/", converters = MessageConverter.class, interceptors = AuthInterceptor.class)
+// @Rest(rootUrl = "http://62.109.17.114/", converters = MessageConverter.class, interceptors = AuthInterceptor.class)
+@Rest(rootUrl = "http://romanitalian.myjino.ru/", converters = MessageConverter.class, interceptors = AuthInterceptor.class)
 public interface RestClient {
     @Get("/auth?login={login}&password={password}")
     AuthResult login(CharSequence login, CharSequence password);
